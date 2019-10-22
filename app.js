63730aceddf917b7f9fdf61e03dc1730c8cbf5bb
rganes01 <rajarajan.ganesan@mssm.edu>
2019-10-21 20:28:49 -0400

    fix reference issue

--- app.js
+++ app.js
@@ -5,19 +5,19 @@
 const http = require('http');
 
 // Load App config
-const app_config = require("./app_config." + (process.env.NODE_ENV || "dev"));
+//const app_config = require("./app_config." + (process.env.NODE_ENV || "dev"));
 
 
 const print_msg = function () {
     console.log("This is a message from the p1-app package");
-    console.log(app_config);
+    //console.log(app_config);
 };
 
 print_msg();
 
 // Datatype module
-const js_dtype = require("./js_basic/js_dtype");
-js_dtype.print_dtype();
-console.log(js_dtype.v1);
+//const js_dtype = require("./js_basic/js_dtype");
+//js_dtype.print_dtype();
+//console.log(js_dtype.v1);
 
 http.createServer( (req,res) => { res.write(JSON.stringify(req.headers)); res.end();  } ).listen(5000);
